d60fda5b320c45bdff8d9645c5a40bde3a1af165
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2020-01-10 14:48:01 +0700

    Update rehearsal_1.cpp

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<string>
 
+using namespace std ;
+
+int main() {
+	float s=0;
+	string n="";
+	
+	cout<<"What is your name ? : ";
+	cin>> n ;
+	cout<<"What is your GPA ? : ";
+	cin>>s ;
+	
+	if (s>=3.5)
+	{
+		cout<<n<<" Inw Jrim Jrim!!!";
+	}
+	else
+	{
+		cout<<"Try harder, "<<n;
+	}
+	
+	return 0 ;
+}
