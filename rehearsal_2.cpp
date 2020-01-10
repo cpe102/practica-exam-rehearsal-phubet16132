8f87eb00b369030cbf498a5bea416583e03e7b1a
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2020-01-10 14:56:47 +0700

    Update rehearsal_2.cpp

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
 
+using namespace std ;
+
+int main(){
+	float sum = 0 ;
+	int x=0;
+	
+	cout<<"Enter x : ";
+	cin>>x;
+	
+	if(x==0){
+		;
+	}
+	
+	while(x>0){
+		sum=sum+x;
+		cout<<"Enter x : ";
+		cin>>x;
+	}
+	
+	cout<<"sum = "<<sum ;
+	
+	return 0 ;
+}
